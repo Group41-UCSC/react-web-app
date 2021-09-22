95def72361c085af1a86e9bc6a3a2733ab85bc3f
shifnasm <56575819+shifnasm@users.noreply.github.com>
2021-09-22 14:14:46 +0530

    Nav Bar list for all users- user Nav bar Linked to dashboard and event user view page
    Nav Bar list for all users and user Nav bar Linked to usr dashboard and event user view page

--- frontend/src/Public/components/listItemsIms.js
+++ frontend/src/Public/components/listItemsIms.js
@@ -4,13 +4,16 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import DashboardIcon from '@material-ui/icons/Dashboard';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
 import PeopleIcon from '@material-ui/icons/People';
-import BarChartIcon from '@material-ui/icons/BarChart';
-import LayersIcon from '@material-ui/icons/Layers';
+import FlagIcon from '@material-ui/icons/Flag';
+import AddCommentIcon from '@material-ui/icons/AddComment';
+import EventIcon from '@material-ui/icons/Event';
+import EmojiEventsIcon from '@material-ui/icons/EmojiEvents';
+import DateRangeIcon from '@material-ui/icons/DateRange';
 import { Link } from 'react-router-dom';
 
-export const mainListItems = (
+export const mainListItemsIms = (
     <div>
         <ListItem button component={Link} to="/dashboard">
             <ListItemIcon>
@@ -20,50 +23,50 @@ export const mainListItems = (
         </ListItem>
         <ListItem button component={Link} to="/view-users">
             <ListItemIcon>
-                <ShoppingCartIcon />
+                <PeopleIcon />
             </ListItemIcon>
             <ListItemText primary="Users" />
         </ListItem>
         <ListItem button component={Link} to="/newevent">
             <ListItemIcon>
-                <PeopleIcon />
+                <EventIcon />
             </ListItemIcon>
             <ListItemText primary="Events" />
         </ListItem>
         <ListItem button component={Link} to="/request-inventory">
             <ListItemIcon>
-                <LayersIcon />
+                <AddCommentIcon />
             </ListItemIcon>
             <ListItemText primary="Inventory" />
         </ListItem>
         
         <ListItem button component={Link} to="/badge-view">
             <ListItemIcon>
-                <LayersIcon />
+                <EmojiEventsIcon />
             </ListItemIcon>
             <ListItemText primary="BatchWork" />
         </ListItem>
         <ListItem button component={Link} to="#">
             <ListItemIcon>
-                <BarChartIcon />
+                <FlagIcon />
             </ListItemIcon>
             <ListItemText primary="Tasks" />
         </ListItem>
         <ListItem button component={Link} to="#">
             <ListItemIcon>
-                <BarChartIcon />
+                <AssignmentTurnedInIcon />
             </ListItemIcon>
             <ListItemText primary="Evaluation" />
         </ListItem>
     </div>
 );
 
-export const secondaryListItems = (
+export const secondaryListItemsIms = (
     <div>
         {/* <ListSubheader inset>Saved reports</ListSubheader> */}
         <ListItem button component={Link} to="/inventory">
             <ListItemIcon>
-                <AssignmentIcon />
+                <DateRangeIcon />
             </ListItemIcon>
             <ListItemText primary="Inventory Management" />
         </ListItem>
